ecb7688857dd40256f9b357d106f417f1d19837c
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-04-11 09:06:14 +0200

    Further stubbed some functions

--- lib/vissen/graphics/context.rb
+++ lib/vissen/graphics/context.rb
@@ -4,9 +4,20 @@ module Vissen
     #
     #
     class Context
-      def initialize(vixel_grid); end
+      def initialize(vixel_stack)
+        @params = {}
+        # Create a mixer for each vixel layer
+        @mixers = vixel_stack.layers.map { |layer| Mixer.new layer }
+      end
 
-      def create_effect(layer, effect_klass, **effect_config); end
+      def create_effect(layer, effect_klass,
+                        mix: [0.0, 1.0], **effect_config)
+        @mixers[layer].create_effect(effect_klass, mix, **effect_config)
+      end
+
+      def attach(label, configurable)
+        @params[label] = configurable
+      end
 
       def configure(*params, value); end
 
