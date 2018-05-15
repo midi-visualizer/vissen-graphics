7ff154ae4569709369a83bb6c23e3e6a4c443289
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-05-15 15:45:07 +0200

    Wrapped in a describe block

--- test/vissen/graphics/modulator_test.rb
+++ test/vissen/graphics/modulator_test.rb
@@ -5,21 +5,21 @@ require 'test_helper'
 describe Vissen::Graphics::Modulator do
   subject { Vissen::Graphics::Modulator }
 
-  it 'works as expected' do
-    mod_a = subject::Bias.new setup: { input: 0.2 }
-    mod_b = subject::Bias.new setup: {
-      offset: 0.5,
-      amplitude: 2.0,
-      input: mod_a
-    }
-    mod_c = subject::Splitter.new setup: { input_a: mod_a, input_b: mod_b }
+  describe 'chaining' do
+    it 'works as expected' do
+      mod_a = subject::Bias.new setup: { input: 0.2 }
+      mod_b = subject::Bias.new setup: {
+        offset: 0.5,
+        amplitude: 2.0,
+        input: mod_a
+      }
+      mod_c = subject::Splitter.new setup: { input_a: mod_a, input_b: mod_b }
 
-    mod_c.tainted?
+      mod_c.tainted?
 
-    assert_equal 0.2, mod_a.value
-    assert_equal 0.9, mod_b.value
-
-    mod_b.untaint!
-    refute mod_a.tainted?
+      assert_equal 0.2, mod_a.value
+      assert_equal 0.9, mod_b.value
+      assert_equal [0.2, 0.9], mod_c.value
+    end
   end
 end
