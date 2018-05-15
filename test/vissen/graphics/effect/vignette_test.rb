28e95927322c026f3c87433c4a72b86eb4bf5c8a
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-05-15 15:44:34 +0200

    Prepared for implementation

--- test/vissen/graphics/effect/vignette_test.rb
+++ test/vissen/graphics/effect/vignette_test.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-# require 'test_helper'
-#
-# describe Vissen::Graphics::Effect::Vignette do
-#   subject { Vissen::Graphics::Effect::Vignette }
-#
-#   let(:rows)     { 2 }
-#   let(:columns)  { 3 }
-#   let(:context)  { Vissen::Output::Context::Grid.new rows, columns }
-#   let(:grid)     { Vissen::Output::VixelBuffer.new context }
-#   let(:effect)   { subject.new context }
-#   let(:t)        { 0 }
-# end
+require 'test_helper'
+
+describe Vissen::Graphics::Effect::Vignette do
+  subject { Vissen::Graphics::Effect::Vignette }
+
+  let(:rows)     { 2 }
+  let(:columns)  { 3 }
+  let(:context)  { Vissen::Output::Context::Grid.new rows, columns }
+  let(:grid)     { Vissen::Output::VixelBuffer.new context }
+  let(:effect)   { subject.new context }
+end
