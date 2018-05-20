b0725d9207f974ab60084479fde2a37bb0437fda
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-05-20 10:43:58 +0200

    Cleaned up test

--- test/vissen/graphics/engine_test.rb
+++ test/vissen/graphics/engine_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 describe Vissen::Graphics::Engine do
   subject { Vissen::Graphics::Engine }
-  
+
   let(:context) { Vissen::Output::Context::Grid.new 2, 3 }
   let(:stack)   { Vissen::Output::VixelStack.new context, 1 }
   let(:mod_r)   { Vissen::Graphics::Modulator::Ramp.new }
@@ -12,7 +12,7 @@ describe Vissen::Graphics::Engine do
     Vissen::Graphics::Effect::Gradient.new setup: { angle: mod_r }
   end
   let(:mixer) do
-    mixer = Vissen::Graphics::Mixer::Absolute.new stack.layers[0], setup: {
+    Vissen::Graphics::Mixer::Absolute.new stack.layers[0], setup: {
       effect: effect,
       p: 1.0
     }
@@ -28,15 +28,15 @@ describe Vissen::Graphics::Engine do
       assert_equal 1.5, mod_r.params.t
     end
   end
-  
+
   describe '#render' do
-    it 'renders the mixers' do      
+    it 'renders the mixers' do
       engine.render 0.0
-      values_before = stack.layers[0].elements.map { |v| v.p }
-            
+      values_before = stack.layers[0].elements.map(&:p)
+
       engine.render 0.1
-      values_after = stack.layers[0].elements.map { |v| v.p }
-      
+      values_after = stack.layers[0].elements.map(&:p)
+
       refute_equal values_before, values_after
     end
   end
